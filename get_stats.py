ed9ec06686a97c11e8575237e2ee1bf089c7dd87
TagCopperLight <tagcopperlight@gmail.com>
2024-01-06 18:21:09 +0100

    Remove unused classes and functions

---
+++ get_stats.py
@@ -0,0 +1,32 @@
+import enum
+
+
+class Vocabulary(enum.Enum):
+    TOP = 0
+    JUNGLE = 1
+    MID = 2
+    ADC = 3
+    SUPPORT = 4
+    RED = 0
+    BLUE = 1
+
+
+class TimeFrame:
+    def __init__(self, length):
+        self.length = length
+        self.interactions = [[(False, False) for _ in range(5)] for _ in range(5)]
+        self.deaths = [[False for _ in range(5)] for _ in range(2)]
+
+
+class Game:
+    def __init__(self):
+        self.id = None
+        self.game_id = None
+        self.duration = None
+        self.date = None
+        self.winner = None
+
+        self.time_frames = []
+
+    def __repr__(self):
+        return f'Game(id={self.id}, game_id={self.game_id}, duration={self.duration}, date={self.date}, winner={self.winner})'
--- run_replays.py
+++ run_replays.py
@@ -21,38 +21,6 @@ def add_done_game(game_id):
     with open('get_data/data/done.json', 'w') as file:
         json.dump(done_games, file, indent=4)
 
-
-class Vocabulary(enum.Enum):
-    TOP = 0
-    JUNGLE = 1
-    MID = 2
-    ADC = 3
-    SUPPORT = 4
-    RED = 0
-    BLUE = 1
-
-
-class TimeFrame:
-    def __init__(self, length):
-        self.length = length
-        self.interactions = [[(False, False) for _ in range(5)] for _ in range(5)]
-        self.deaths = [[False for _ in range(5)] for _ in range(2)]
-
-
-class Game:
-    def __init__(self):
-        self.id = None
-        self.game_id = None
-        self.duration = None
-        self.date = None
-        self.winner = None
-
-        self.time_frames = []
-
-    def __repr__(self):
-        return f'Game(id={self.id}, game_id={self.game_id}, duration={self.duration}, date={self.date}, winner={self.winner})'
-
-
 def main():
     done_games = get_done_games()
     data = get_saved_games()
