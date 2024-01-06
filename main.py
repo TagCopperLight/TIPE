747589b0bc48bea15b5b3094c3430f268562310c
TagCopperLight <tagcopperlight@gmail.com>
2024-01-06 21:05:29 +0100

    Refactor code to improve readability and maintainability

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
+import pathlib
+import json
+import pickle
 import networkx as nx
 import matplotlib.pyplot as plt
 from C45.c45.c45 import C45
-from run_replays import get_games, Game
 
+from get_stats import Game, TimeFrame
+
+
+def get_games():
+    done_objects_path = pathlib.Path('game_objects/done.json')
+    with open(done_objects_path, 'r') as file:
+        done_objects = json.load(file)
+    
+    games = []
+    for game_id in done_objects:
+        object_path = pathlib.Path(f'game_objects/{game_id[5:]}.pkl')
+        with open(object_path, 'rb') as file:
+            games.append(pickle.load(file))
+
+    return games
 
 def show_graph(graph):
     pos = nx.nx_agraph.graphviz_layout(G)
@@ -24,31 +41,34 @@ def get_metrics(graph):
 
     return indegrees, outdegrees, nx.closeness_centrality(graph), nx.betweenness_centrality(graph), nx.eigenvector_centrality(graph)
 
-def create_graph_from_game(game: Game, time_frame: int):
+def create_graph_from_game(game: Game, time_frame):
     G = nx.DiGraph()
     for team in [1, 2]:
         for role in range(1, 6):
             G.add_node(f"T{team}-R{role}")
     G.add_node("DEATH")
     
-    for blue_player, data in enumerate(game.time_frames[time_frame]):
+    for blue_player, data in enumerate(game.time_frames[time_frame].interactions):
         for red_player, data2 in enumerate(data):
-            if data2[0] == 'TRUE':
+            if data2[0]:
                 G.add_edge(f"T1-R{red_player+1}", f"T2-R{blue_player+1}")
-            if data2[1] == 'TRUE':
+            if data2[1]:
                 G.add_edge(f"T2-R{blue_player+1}", f"T1-R{red_player+1}")
     
-    for player, data in enumerate(game.deaths_frames[time_frame]):
-        if data == 'TRUE':
-            if player < 5:
-                G.add_edge(f"T1-R{player+1}", "DEATH")
-            else:
-                G.add_edge(f"T2-R{player-4}", "DEATH")
+    for team, data in enumerate(game.time_frames[time_frame].deaths):
+        for player, data in enumerate(data):
+            if data:
+                    G.add_edge(f"T{team+1}-R{player+1}", "DEATH")
     return G
 
+def main():
+    games: list[Game] = get_games()
 
-for i in range(1):
-    print(f"Time frame {i}")
-    G = create_graph_from_game(get_games()[0], 13)
-    get_metrics(G)
-    # show_graph(G)
+    for i in range(1):
+        print(f"Time frame {i}")
+        G = create_graph_from_game(games[0], 13)
+        get_metrics(G)
+        # show_graph(G)
+
+if __name__ == '__main__':
+    main()
