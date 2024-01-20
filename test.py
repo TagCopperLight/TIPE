a7fa9ec88d382218ec7cce92141d6bcaaef85861
TagCopperLight <tagcopperlight@gmail.com>
2024-01-20 23:29:32 +0100

    Refactor feature labels and role assignment in test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 features = [
-    [['cls', 1, 12], ['eige', 0, 10], ['eige', 6, 7], ['eige', 9, 14], ['eige', 10, 6]]
+    [['outdeg', 0, 12], ['outdeg', 4, 11], ['cls', 8, 2], ['btw', 9, 8], ['eige', 10, 9]]
 ]
 
 for feature_list in features:
@@ -7,10 +7,10 @@ for feature_list in features:
     for feature in feature_list:
         print('(', end='')
         team, role = feature[1]%2, feature[1]//2
-        if role == 5:
+        if feature[1] == 10:
             print(f"'{feature[0]}', 'DEATH', {feature[2]}", end='')
         else:
-            print(f"'{feature[0]}', 'T{team+1}-R{role+1}', {feature[2]}", end='')
+            print(f"'{feature[0]}', '{['T1-R1', 'T1-R2', 'T1-R3', 'T1-R4', 'T1-R5', 'T2-R1', 'T2-R2', 'T2-R3', 'T2-R4', 'T2-R5'][feature[1]]}', {feature[2]}", end='')
         if feature != feature_list[-1]:
             print('), ', end='')
         else:
